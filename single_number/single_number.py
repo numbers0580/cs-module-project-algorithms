323d69eaafd18261c402cf02cbcea51ac0aece0c
numbers0580 <koreankid18@yahoo.com>
2020-09-16 14:47:50 -0600

    Completed single_number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,8 +4,20 @@ Returns: an integer
 '''
 def single_number(arr):
     # Your code here
+    # So both the array below and the constructed one in the test file are not sorted
+    # I think a portion of the count sort method would apply here
 
-    pass
+    count = [0] * len(arr) # I know this is practically twice as long as we need, but I'll ignore the 0's at the end
+
+    for i in arr:
+        count[i] += 1
+
+    pos = 0
+    while count[pos] != 1 and pos < len(arr):
+        pos += 1
+
+    return pos
+
 
 
 if __name__ == '__main__':
