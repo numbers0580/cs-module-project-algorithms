874a5bff5f946ed5bb3fb13621dc4ac31ebac715
numbers0580 <koreankid18@yahoo.com>
2020-09-16 18:16:22 -0600

    Cookies!

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,11 +1,29 @@
+import math
+
 '''
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+def eating_cookies(n, arr=[]):
     # Your code here
+    total = 0
 
-    pass
+    if n < 3:
+        total = math.factorial(n)
+    else:
+        a = math.factorial(0) # 1
+        b = math.factorial(1) # 1
+        c = math.factorial(2) # 2
+        # Note: Simple factorials won't work beyond n = 2
+        for i in range(n-2):
+            total = a + b + c
+            # Shift a, b, and c for next iteration of for-loop
+            a = b
+            b = c
+            c = total
+
+    return total
+    
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,6 +4,10 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
+    # If I understand the README, I may need a temporary array to house only the non-zero integers
+    # I can't pop the zeroes out of arr because it appears the README shows the original array length doesn't change
+
+    temp = []
 
     pass
 
