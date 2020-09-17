96315e76ee48a3afe85da99d30ba97213e15fbc4
numbers0580 <koreankid18@yahoo.com>
2020-09-16 18:47:30 -0600

    Cleaned up cookie crumbs.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,26 +1,20 @@
-import math
-
 '''
 Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n, arr=[]):
     # Your code here
-    total = 0
+    a = 0
+    b = 0
+    c = 1
+    total = 1
 
-    if n < 3:
-        total = math.factorial(n)
-    else:
-        a = math.factorial(0) # 1
-        b = math.factorial(1) # 1
-        c = math.factorial(2) # 2
-        # Note: Simple factorials won't work beyond n = 2
-        for i in range(n-2):
-            total = a + b + c
-            # Shift a, b, and c for next iteration of for-loop
-            a = b
-            b = c
-            c = total
+    for i in range(n):
+        total = a + b + c
+        # Shift a, b, and c for next iteration of for-loop
+        a = b
+        b = c
+        c = total
 
     return total
 
